e03cabc72dfccb0315464a6e0b94928a59404f6d
Alex Curran <alexcurran.va@gmail.com>
2021-10-11 12:21:52 -0500

    Add indent rule

--- src/App.js
+++ src/App.js
@@ -6,55 +6,55 @@ import Tours from "./components/Tours";
 const API_URL = 'https://course-api.com/react-tours-project';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [tours, setTours] = useState([]);
 
-  async function getTours() {
-    try {
-      const response = await fetch(API_URL);
-      const data = await response.json();
+    async function getTours() {
+        try {
+            const response = await fetch(API_URL);
+            const data = await response.json();
       
-      setLoading(false);
-      setTours(data);
+            setLoading(false);
+            setTours(data);
       
-    } catch (err) {
-      setLoading(false);
-      console.error(err);
+        } catch (err) {
+            setLoading(false);
+            console.error(err);
+        }
     }
-  }
 
-  function removeTour(id) {
-    const newTours = tours.filter((tour) => tour.id !== id);
+    function removeTour(id) {
+        const newTours = tours.filter((tour) => tour.id !== id);
 
-    setTours(newTours);
-  }
+        setTours(newTours);
+    }
 
-  useEffect(() => {
-    getTours();
-  }, []);
+    useEffect(() => {
+        getTours();
+    }, []);
 
-  if (loading) {
+    if (loading) {
+        return (
+            <main>
+                <Loading />
+            </main>
+        );
+    }
+
+    if (tours.length === 0) {
+        return (
+            <main>
+                <h2>No Tours Left</h2>
+                <button onClick={getTours} className="btn">Refresh Tours</button>
+            </main>
+        );
+    }
+
     return (
-      <main>
-        <Loading />
-      </main>
+        <main>
+            <Tours tours={tours} removeTour={removeTour} />
+        </main>
     );
-  }
-
-  if (tours.length === 0) {
-    return (
-      <main>
-        <h2>No Tours Left</h2>
-        <button onClick={getTours} className="btn">Refresh Tours</button>
-      </main>
-    );
-  }
-
-  return (
-    <main>
-      <Tours tours={tours} removeTour={removeTour} />
-    </main>
-  );
 }
 
 export default App;
